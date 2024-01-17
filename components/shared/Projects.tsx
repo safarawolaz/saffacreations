ed709accbbe941645023913ba6cb10f201012abf
safarawolaz <safarawolaz@yahoo.com>
2024-01-17 20:40:00 +0200

    a tag

--- components/shared/Projects.tsx
+++ components/shared/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
                 Our Most Recent <br /> Projects
             </h3>
             <div className="grid sm:grid-cols-2 lg:grid-cols-3  w-full gap-8 lg:h-[400px] sm:h-[800px] h-[1200px]">
-                <Link href="https://divineacademy.co.za/" target="_blank" rel="noopener noreferrer"   className="bg-black rounded-2xl overflow-hidden relative  transition-all duration-300 w-full h-full">
+                <a href="https://divineacademy.co.za/" target="_blank" rel="noopener noreferrer"   className="bg-black rounded-2xl overflow-hidden relative  transition-all duration-300 w-full h-full">
                   <Image src='/divine.png' width={2976} height={3456}  alt='project 1 image' className='hover:scale-110 transition-all duration-300 cursor-pointer z-10 absolute w-full h-full' objectFit='cover'/>
                   <div className="bg-[#c59402]  p-6 rounded-xl absolute z-20 bottom-2 left-2 cursor-pointer shadow-xl hover:-translate-y-2 transition-all duration-300">
                     <h3 className='text-lg font-bold tracking=widest text-white'>
@@ -19,8 +19,8 @@ const Projects = () => {
                       www.divineacademy.co.za
                     </p>
                   </div>
-                </Link>
-                <Link href="https://mxprinters.vercel.app/" target='_blank' rel="noopener noreferrer"  className="bg-black rounded-2xl overflow-hidden relative">
+                </a>
+                <a href="https://mxprinters.vercel.app/" target='_blank' rel="noopener noreferrer"  className="bg-black rounded-2xl overflow-hidden relative">
                   <Image src='/mxprinters.png' width={2000} height={3000}  alt='project 1 image'  className='hover:scale-110 transition-all duration-300 cursor-pointer z-10 absolute' objectFit='cover'/>
                   <div className="bg-[#c59402]  p-6 rounded-xl absolute z-20 bottom-2 left-2 cursor-pointer shadow-xl hover:-translate-y-2 transition-all duration-300">
                     <h3 className='text-lg font-bold tracking=widest text-white'>
@@ -30,7 +30,7 @@ const Projects = () => {
                       www.mxprinters.co.za
                     </p>
                   </div>
-                </Link>
+                </a>
                 <div className="bg-black rounded-2xl overflow-hidden relative">
                   <Image src='/projects3.jpg' width={2000} height={3000}  alt='project 1 image' className='hover:scale-110 transition-all duration-300 cursor-pointer z-10 absolute' objectFit='cover' />
                   <div className="bg-[#c59402]  p-6 rounded-xl absolute z-20 bottom-2 left-2 cursor-pointer shadow-xl hover:-translate-y-2 transition-all duration-300">
