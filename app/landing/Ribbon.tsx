8be8ade08253054e5f0ed5344e8fa6cd4b4f09cd
safarawolaz <safarawolaz@yahoo.com>
2024-01-26 07:01:35 +0200

    addedd text roll

--- app/landing/Ribbon.tsx
+++ app/landing/Ribbon.tsx
@@ -1,13 +1,59 @@
-import React from 'react'
+const roll = [
+  {
+    id: 1,
+    desc: 'life time support'
+  },
+  {
+    id: 2,
+    desc: 'high quality & innovative solutions'
+  },
+  {
+    id: 3,
+    desc: 'robust software solutions'
+  },
+  {
+    id: 4,
+    desc: 'latest tools & technologies'
+  },
+  {
+    id: 5,
+    desc: 'user-centric solutions'
+  },
+  {
+    id: 6,
+    desc: 'enhanced user engagement'
+  },
+  {
+    id: 7,
+    desc: 'enhanced customer experience'
+  },
+  {
+    id: 8,
+    desc: 'AI integration to optimize production'
+  },
+  {
+    id: 9,
+    desc: 'enhanced customer engagement'
+  },
+  {
+    id: 10,
+    desc: 'innovative software products'
+  },
+]
 
 const Ribbon = () => {
   return (
-    <section className='px-4 lg:px-10 bg-[#000000] py-6 w-full flex gap-8 items-center overflow-hidden'>
-        <div className="text-xl text-[#cfcece] font-bold uppercase tracking-wider whitespace-nowrap">*transform ideas into reality</div>
-        <div className="text-xl text-[#cfcece] font-bold uppercase tracking-wider whitespace-nowrap">*transform ideas into reality</div>
-        <div className="text-xl text-[#cfcece] font-bold uppercase tracking-wider whitespace-nowrap">*transform ideas into reality</div>
-        <div className="text-xl text-[#cfcece] font-bold uppercase tracking-wider whitespace-nowrap">*transform ideas into reality</div>
-        <div className="text-xl text-[#cfcece] font-bold uppercase tracking-wider whitespace-nowrap">*transform ideas into reality</div>
+    <section className='px-4 lg:px-10 bg-[#000000] whitespace-nowrap overflow-hidden '>
+      <div className="animate-roll py-6 w-full flex gap-8 items-center ">
+          {
+            roll.map(text => <div key={text.id}>
+              <div className="text-xl text-[#cfcece] font-bold uppercase tracking-wider whitespace-nowrap">
+                *{text.desc}
+              </div>
+            </div>)
+          }
+      </div>
+       
         
     </section>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -77,10 +77,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        roll: {
+          "0%": {transform: "translateX(0%)"},
+          "100%": {transform: "translateX(-200%)"}
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        roll: "roll 24s linear infinite"
       },
     },
   },
